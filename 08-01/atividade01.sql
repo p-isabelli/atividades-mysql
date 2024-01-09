0882157df79ddadf021d6b5a6d3404052e85fa4d
isabelli c pereira <isabellipereira58@gmail.com>
2024-01-09 14:21:34 -0300

    exercicios 08-01

--- 08-01/atividade01.sql
+++ 08-01/atividade01.sql
@@ -17,7 +17,7 @@ VALUES ("Isabelli C Pereira", "12345678900", "2001-05-07", 15000.00, "Desenvolve
 ("Julio Souza", "56749294201", "1973-04-12", 25000.00, "Desenvolvedor Backend Sr"),
 ("Joana da Silva", "12365487633", "1999-02-12", 5000.00, "Desenvolvedora FrontEnd Jr"),
 ("Maria Pereira", "56782345609", "1995-11-08", 12000.00, "Analista de sistemas"),
-("Fernanda Souza", "67309213407", "2003-12-23", 1500.00, "Gestora");
+("Fernanda Silva", "67309213407", "2003-12-23", 1500.00, "Gestora");
 
 SELECT * FROM tb_colaboradores WHERE salario > 2000;
 
